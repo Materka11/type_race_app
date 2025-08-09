8bd508cc6bf08f48188623f6a1162f8efb8a9215
Materka11 <arekmaterka11@gmail.com>
2025-08-09 11:00:33 +0200

    Feature: Add additional models

---
+++ apps/backend/Models/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models
+{
+    public class Player
+    {
+        public string ConnectionId { get; set; } = "";
+        public string Name { get; set; } = "";
+        public int Score { get; set; } = 0;
+    }
+}
